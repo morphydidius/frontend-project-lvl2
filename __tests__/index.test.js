e5e7cb902ffd06a66f089d6eb72f5760da321035
mdidius <morphydidius@gmail.com>
2020-10-06 00:31:21 +0300

    linter fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,14 +5,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import gendiff from '../index.js';
 
-let __filename, __dirname;
-
-beforeAll(() => {
-  __filename = fileURLToPath(import.meta.url);
-  __dirname = path.dirname(__filename);
-});
-
 test.each([['json'], ['yaml'], ['ini']])('comparing %s files', (ext) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
   const result = gendiff(path.join(__dirname, '..', '__fixtures__', `file1.${ext}`), path.join(__dirname, '..', '__fixtures__', `file2.${ext}`));
   const rightAnswer = fs.readFileSync(path.join(__dirname, '..', '__fixtures__', 'file1file2.txt'), 'utf8');
   expect(result).toBe(rightAnswer);
